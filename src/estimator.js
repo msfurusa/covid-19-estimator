ff5ea22b469369f53981327761a36c32d041e3fa
Milton Furusa <msfurusa@gmail.com>
2020-04-11 10:47:18 +0200

    Impact Estimator

--- src/estimator.js
+++ src/estimator.js
@@ -1,10 +1,21 @@
 const covid19ImpactEstimator = (data) => {
-    const input = data;
-    return {
+    const input = JSON.parse(JSON.stringify(data));
+    const impactObj = {};
+    const severImpactObj = {};
+    const impactResult = impactCalc(input);
+    const severeImpactResult = severeImpactCalc(input);
+    impactObj.currentlyInfected = impactResult.currentlyInfected;
+    impactObj.infectionsByRequestedTime = impactResult.InfectionsByTime;
+    severeImpactObj.currentlyInfected = severeImpactResult.currentlyInfected;
+    severeImpactObj.infectionsByRequestedTime = severeImpactResult.InfectionsByTime;
+    const output = {
         data: input,
-        impact: {},
-        severeImpact: {}
+        estimate: {
+            impact: impactObj,
+            severeImpact: severeImpactObj
+        }
     };
+    return (JSON.stringify(output));
 };
 
 export default covid19ImpactEstimator;
